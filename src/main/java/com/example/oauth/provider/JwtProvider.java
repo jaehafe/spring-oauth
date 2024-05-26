489d18ef06e5dd9deca6e0f3fbe6fd015308195b
Jaeha Lee <wogkdkrm112@gmail.com>
2024-05-26 20:07:39 +0900

    chore: minor update

--- src/main/java/com/example/oauth/provider/JwtProvider.java
+++ src/main/java/com/example/oauth/provider/JwtProvider.java
@@ -1,6 +1,5 @@
 package com.example.oauth.provider;
 
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
